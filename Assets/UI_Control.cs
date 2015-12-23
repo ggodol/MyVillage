7a29539891c388b1f8e3b433797da73e9600720d
ggodol <gghost10@naver.com>
2015-12-24 02:39:05 +0900

    UI Test

---
+++ Assets/UI_Control.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI_Control : MonoBehaviour {
+    public GameObject Facilities_List_Panel;
+
+    // Use this for initialization
+    void Start () {
+        Facilities_List_Panel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
